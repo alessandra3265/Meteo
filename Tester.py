02109bbd3c76cbcc53e31f4a3b0a478166723476
alessandra3265 <61121157+alessandra3265@users.noreply.github.com>
2020-05-18 20:14:19 +0200

    Add files via upload

--- Tester.py
+++ Tester.py
@@ -17,7 +17,7 @@ query scrive i risultati nel file filename.csv
 TEST 1 
 """
 medie = "3"         #mensili
-intervallo = "3"    #ultimi 30 giorni
+intervallo = "4"    #a scelta
 giorno = "1" 
 mese = "1"          #gennaio
 anno = "2020"
@@ -25,7 +25,7 @@ giornoFine = "1"
 meseFine = "3"      #marzo
 annoFine = "2020" 
 
-query(medie, intervallo, giorno, mese, anno, giornoFine, meseFine, annoFine, "out.csv")
+query(medie, intervallo, giorno, mese, anno, giornoFine, meseFine, annoFine, "test1.csv")
 
 """
 TEST 2
@@ -33,17 +33,13 @@ TEST 2
 medie = "1"         #orari
 intervallo = "2"    #ultimi 7 giorni
 
-query(medie, intervallo, giorno, mese, anno, giornoFine, meseFine, annoFine, "out.csv")
+query(medie, intervallo, giorno, mese, anno, giornoFine, meseFine, annoFine, "test2.csv")
 
 """
 TEST 3 
 """
 medie = "2"         #giornalieri
 intervallo = "3"    #ultimi 30 giorni
-giorno = "1" 
-mese = "1"          #gennaio
-anno = "2020"
-giornoFine = "1" 
-meseFine = "3"      #marzo
-annoFine = "2020" 
-query(medie, intervallo, giorno, mese, anno, giornoFine, meseFine, annoFine, "out.csv")
+
+query(medie, intervallo, giorno, mese, anno, giornoFine, meseFine, annoFine, "test3.csv")
+
