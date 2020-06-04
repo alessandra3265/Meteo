283913d1058a6c3c5fa991a7ee84fe191ac8ffa9
alessandra3265 <alessandra3266@gmail.com>
2020-06-04 10:04:01 +0200

    aggiunto script che legge un file pdf in locale con Camelot

---
+++ pdf/umbriaCamelot.py
@@ -0,0 +1,12 @@
+import camelot
+
+path = "C:\\Users\\Alessandra\\Downloads\\DATI PIOGGIA 2019.pdf"
+tables = camelot.read_pdf(path,pages='1', multiple_tables = True, flavor='stream', strip_text='\n')
+print(tables)
+print(tables[0].df)
+tables[0].to_excel('umbria2019.xlsx')
+#tables[0].to_csv('umbria2019.csv')
+
+
+
+
