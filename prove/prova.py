bd04da2c62d2a1eeb3df180fd2fbe6fecafe4054
alessandra3265 <61121157+alessandra3265@users.noreply.github.com>
2020-07-08 15:45:17 +0200

    Delete prova.py

--- prove/prova.py
+++
@@ -1,9 +0,0 @@
-s = "2020/0019_2020_PREC.htm"
-result = s.split('/')
-print(result)
-res = result[1].split('_', 1)
-print(res)
-
-s1 = "Molini (Laghi) (191)"
-result = s1.split(' (')
-print(result[0])
